1c93f030394a2a696e628876443265e4437f83b0
JoseCabreraJC <josem.cabrera259@gmail.com>
2022-01-09 20:49:43 -0300

    agregada config de truffle, migraciones y contratos

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract Tether {
+    string public name = 'Tether';
+    string public symbol = 'USDT';
+    uint public totalSupply = 100_000_000_000_000_000_000_000_000;
+    uint public decimals = 18;
+}
