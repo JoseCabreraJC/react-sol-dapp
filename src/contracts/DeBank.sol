5cf4b12fb6fba875d53d14997df9994b459f3c43
JoseCabreraJC <josem.cabrera259@gmail.com>
2022-01-13 23:15:23 -0300

    agregada logica para trasnferir de una cuenta al banco

--- src/contracts/DeBank.sol
+++ src/contracts/DeBank.sol
@@ -10,8 +10,29 @@ contract DeBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
     }
+    function depositToken(uint256 _amount){
+        require(_amount >= 0, 'amount cannot be 0')
+
+        tether.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if(!hasStaked) {
+            stakers.push[msg.sender];
+        }
+
+        ifStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+
+    }
+
 }
