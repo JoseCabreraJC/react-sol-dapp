34fb267bfe703e33fbed819602b34501a9367f2c
JoseCabreraJC <josem.cabrera259@gmail.com>
2022-01-15 17:06:44 -0300

    agregado Navbar y estilos de app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import './App.css'
+import Navbar from './Navbar';
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      account: '0x0'
+    }
+  }
+  
   render() {
     return (
-      <div className='text-center'>
-        <h1>Hi World!</h1>
+      <div>
+        <Navbar account={this.state.account}/>
+        <div className='text-center green-font'>
+          <h1></h1>
+        </div>
       </div>
     );
   }
