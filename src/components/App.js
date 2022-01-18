fbde4956af3e3c2a7e5b5175b4384a66b811cca7
JoseCabreraJC <josem.cabrera259@gmail.com>
2022-01-18 08:47:41 -0300

    added web 3 onMount loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import './App.css'
 import Navbar from './Navbar';
-
+import Web3 from 'web3';
 
 class App extends Component {
+
+  async UNSAFE_componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+
+  }
+
+  async loadWeb3(){
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('no Ethereum browser detected. Why not check Metamask?')
+    }
+  }
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const account = await web3.eth.getAccounts()
+    console.log(account)
+  }
+
   constructor(props){
     super(props)
     this.state = {
