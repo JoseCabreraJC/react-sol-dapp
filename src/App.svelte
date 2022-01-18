319c9f9d4767fa8771bb746855505ca964561995
JoseCabreraJC <josem.cabrera259@gmail.com>
2022-01-17 21:17:58 -0300

    edited project to work with svelte isntead of react

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+  import Navbar from './lib/Navbar.svelte'
 </script>
 
 <main>
+  <Navbar />
   <img src={logo} alt="Svelte Logo" />
   <h1>Hello world!</h1>
 
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    let src = '../'
+</script>
+
+<nav class="navbar navbar-dark fix-top shadow p-o">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="">
+        <img {src} alt=""> &nbsp; DAPP Yield farming (Decentralized Banking)
+    </a>
+    
+</nav>
+
+<style>
+    nav {
+        background-color: black;
+        height: 50px;
+    }
+</style>
