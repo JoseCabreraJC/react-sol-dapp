33a3f82c48792e6f2bf09588958e92eaf495c616
JoseCabreraJC <josem.cabrera259@gmail.com>
2022-02-16 09:27:47 -0300

    added stakeTokens function in the frontend

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,12 +20,20 @@ class Main extends Component {
               </tbody>
           </table>
           <div className='card mb-2' style={{opacity:'0.9'}}>
-              <form className='mb-3'>
+              <form 
+              onSubmit={(event) => {
+                event.preventDefault()
+                let amount = this.input.value.toString()
+                amount = window.web3.utils.toWei(amount, 'Ether')
+                this.props.stakeTokens(amount)
+              }}
+              className='mb-3'>
                   <div style={{borderSpacing: '0 1em'}}>
                       <label className='float-left' style={{marginLeft: '15px'}}><b>Stake tokens</b></label>
                       <span className='float-right' style={{marginRight:'8px'}}>Balance: {window.web3.utils.fromWei(this.props.tetherBalance, 'Ether')}</span>
                       <div className='input-group mb-4'>
                           <input
+                          ref={(input) => {this.input = input}}
                           type='text'
                           placeholder='0'
                           required/>
