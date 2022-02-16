ad82668e7a1255295c4f9fe9b13eec8f402d0b67
JoseCabreraJC <josem.cabrera259@gmail.com>
2022-02-16 09:32:43 -0300

    added unstakeTokens function in the frontend

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,10 +22,10 @@ class Main extends Component {
           <div className='card mb-2' style={{opacity:'0.9'}}>
               <form 
               onSubmit={(event) => {
-                event.preventDefault()
-                let amount = this.input.value.toString()
-                amount = window.web3.utils.toWei(amount, 'Ether')
-                this.props.stakeTokens(amount)
+                event.preventDefault();
+                let amount = this.input.value.toString();
+                amount = window.web3.utils.toWei(amount, 'Ether');
+                this.props.stakeTokens(amount);
               }}
               className='mb-3'>
                   <div style={{borderSpacing: '0 1em'}}>
@@ -47,7 +47,13 @@ class Main extends Component {
                       <button type='submit' className='btn btn-primary btn-lg btn-block'>DEPOSIT</button>
                   </div>
               </form>
-            <button className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
+            <button 
+            type='submit'
+            onClick={(event) => {
+              event.preventDefault();
+              this.props.unstakeTokens();
+            }}
+            className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
             <div className='card-body text-center' style={{color:'blue'}}>
                 Air drop
             </div>
